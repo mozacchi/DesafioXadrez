0b594d43baf5f515afa8ce29928a3983301ad8fb
Bruno Amorim <oultimoplayer@gmail.com>
2025-03-17 18:45:58 +0000

    xadrezdoamorim

--- xadrez.c
+++ xadrez.c
@@ -4,4 +4,30 @@ int main(void){
 
     printf("Desafio Jogo de Xadrez\n");
 
+}    printf("Movimento da Torre:\n");
+for (int i = 1; i <= 5; i++) {
+    printf("Direita\n");
+}
+
+printf("\n");
+
+
+printf("Movimento do Bispo:\n");
+int j = 1;
+while (j <= 5) {
+    printf("Cima, Direita\n");
+    j++;
+}
+
+printf("\n");
+
+
+printf("Movimento da Rainha:\n");
+int k = 1;
+do {
+    printf("Esquerda\n");
+    k++;
+} while (k <= 8);
+
+return 0;
 }
